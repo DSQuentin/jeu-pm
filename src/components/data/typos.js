900d1c353c5db7757c8885ea788aa03aee99584c
Quentin <dsquentin@hotmail.fr>
2023-01-10 13:37:16 +0100

    Ajout de toutes les typos

--- src/components/data/typos.js
+++ src/components/data/typos.js
@@ -1,7 +1,130 @@
 export const typos = {
-  "Harry Potter": "./data/typos/HP.PNG",
-  "Jurassic Parc": "./data/typos/Jurassic Parc.PNG",
-  "Retour vers le futur": "./data/typos/Retour vers le futur.PNG",
-  "Star Wars": "./data/typos/Star Wars.PNG",
-  "Seigneur des Anneaux": "./data/typos/Seigneur des Anneaux.PNG",
+  1: {
+    title: "L'age de glace",
+    image: "image/typos/agedeglace.PNG",
+  },
+  2: {
+    title: "Alice aux pays des merveilles",
+    image: "image/typos/aliceauxpaysdesmerveilles.PNG",
+  },
+  3: {
+    title: "Alien",
+    image: "image/typos/alien.PNG",
+  },
+  4: {
+    title: "Apocalypse Now",
+    image: "image/typos/apocalypsenow.PNG",
+  },
+  5: {
+    title: "Austin Power",
+    image: "image/typos/austinpower.PNG",
+  },
+  6: {
+    title: "Batman Forever",
+    image: "image/typos/batmanforever.PNG",
+  },
+  7: {
+    title: "Blade",
+    image: "image/typos/blade.PNG",
+  },
+  8: {
+    title: "Casper",
+    image: "image/typos/casper.PNG",
+  },
+  9: {
+    title: "Casper",
+    image: "image/typos/casper.PNG",
+  },
+  10: {
+    title: "Fight Club",
+    image: "image/typos/fightclub.PNG",
+  },
+  11: {
+    title: "Harry Potter",
+    image: "image/typos/harrypotter.PNG",
+  },
+  12: {
+    title: "Hell Boy",
+    image: "image/typos/hellboy.PNG",
+  },
+  13: {
+    title: "Indiana Jones",
+    image: "image/typos/indianajones.PNG",
+  },
+  14: {
+    title: "Jurassic Park",
+    image: "image/typos/jurassicpark.PNG",
+  },
+  15: {
+    title: "La famille Addams",
+    image: "image/typos/lafamilleaddams.PNG",
+  },
+  16: {
+    title: "Le parrain",
+    image: "image/typos/leparrain.PNG",
+  },
+  17: {
+    title: "Mars Attack",
+    image: "image/typos/marsattack.PNG",
+  },
+  18: {
+    title: "Matrix",
+    image: "image/typos/matrix.PNG",
+  },
+  19: {
+    title: "Mission Impossible",
+    image: "image/typos/missionimpossible.PNG",
+  },
+  20: {
+    title: "Pirates des Caraibes",
+    image: "image/typos/piratesdescaraibes.PNG",
+  },
+  21: {
+    title: "Resident Evil",
+    image: "image/typos/residentevil.PNG",
+  },
+  22: {
+    title: "Retour vers le Futur",
+    image: "image/typos/retourverslefutur.PNG",
+  },
+  23: {
+    title: "Scream",
+    image: "image/typos/scream.PNG",
+  },
+  24: {
+    title: "Seigneur des Anneaux",
+    image: "image/typos/seigneurdesanneaux.PNG",
+  },
+  25: {
+    title: "Sincity",
+    image: "image/typos/sincity.PNG",
+  },
+  26: {
+    title: "SOS Fantomes",
+    image: "image/typos/sosfantomes.PNG",
+  },
+  27: {
+    title: "Spiderman",
+    image: "image/typos/spiderman.PNG",
+  },
+  28: {
+    title: "Star Trek",
+    image: "image/typos/startrek.PNG",
+  },
+  29: {
+    title: "Star Wars",
+    image: "image/typos/starwars.PNG",
+  },
+  30: {
+    title: "Terminator",
+    image: "image/typos/terminator.PNG",
+  },
+  31: {
+    title: "Transformers",
+    image: "image/typos/transformers.PNG",
+  },
+  32: {
+    title: "Twilight",
+    image: "image/typos/twilight.PNG",
+  },
 };
